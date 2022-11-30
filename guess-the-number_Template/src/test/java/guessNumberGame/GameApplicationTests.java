e1a2bd41f991c47bfa4da483f513fa04d42525de
Victoria Maia <102436597+vicmaia@users.noreply.github.com>
2022-11-30 17:59:32 -0500

    The missing part of the code, now test passed!

---
+++ guess-the-number_Template/src/test/java/guessNumberGame/GameApplicationTests.java
@@ -0,0 +1,10 @@
+package guessNumberGame;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GameApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
